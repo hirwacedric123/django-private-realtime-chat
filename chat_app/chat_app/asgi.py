e41fa6564d6c74539356461f3d30b4759d238320
hirwa cedric <hirwacedr12@gmail.com>
2025-03-06 22:06:03 +0200

    implement WebSocket support for chat functionality with ChatConsumer and routing

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+# asgi.py
+
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # Import the WebSocket routes
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_app.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
